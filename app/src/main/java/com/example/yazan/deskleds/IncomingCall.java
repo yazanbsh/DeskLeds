5a26aaeabf637fa214ffe581565ff9e329ff6d81
Yazan <sowoooo@gmail.com>
2016-05-06 15:51:50 +0300

    recciver

---
+++ app/src/main/java/com/example/yazan/deskleds/IncomingCall.java
@@ -0,0 +1,51 @@
+package com.example.yazan.deskleds;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Yazan on 5/6/2016.
+ */
+public class IncomingCall extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+
+        try {
+            // TELEPHONY MANAGER class object to register one listner
+            TelephonyManager tmgr = (TelephonyManager) context
+                    .getSystemService(Context.TELEPHONY_SERVICE);
+
+            //Create Listner
+            MyPhoneStateListener PhoneListener = new MyPhoneStateListener();
+
+            // Register listener for LISTEN_CALL_STATE
+            tmgr.listen(PhoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        } catch (Exception e) {
+            Log.e("Phone Receive Error", " " + e);
+        }
+
+    }
+
+    private class MyPhoneStateListener extends PhoneStateListener {
+
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            Log.d("MyPhoneListener",state+"   incoming no:"+incomingNumber);
+
+            if (state == 1) {
+
+                String msg = "New Phone Call Event. Incomming Number : "+incomingNumber;
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(pcontext, msg, duration);
+                toast.show();
+
+            }
+        }
+    }
+}
