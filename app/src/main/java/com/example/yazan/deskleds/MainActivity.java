df7ab9fca6d9d473dc0789cdf9e7885a5e7c1227
Yazan <sowoooo@gmail.com>
2016-05-07 17:50:55 +0300

    work on 23

--- app/src/main/java/com/example/yazan/deskleds/MainActivity.java
+++ app/src/main/java/com/example/yazan/deskleds/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.yazan.deskleds;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -12,6 +16,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(getBaseContext(),
+                Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_SMS)) {
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_SMS},1);
+
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(getBaseContext(),
+                Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECEIVE_SMS)) {
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECEIVE_SMS},2);
+
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(getBaseContext(),
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},3);
+
+            }
+        }
     }
 
     public void colorOnclickListner(View view) {
