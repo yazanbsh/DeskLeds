29959e7f6ab7f4a64bab79eb8cd99119c224bc4e
Yazan <sowoooo@gmail.com>
2016-05-17 14:09:15 +0300

    blue start

--- app/src/main/java/com/example/yazan/deskleds/MainActivity.java
+++ app/src/main/java/com/example/yazan/deskleds/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.yazan.deskleds;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -8,14 +9,28 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private BluetoothAdapter myBluetooth = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Perm_check();
+        Perm_bluetooth();
+        blue_init();
 
+    }
+
+    public void colorOnclickListner(View view) {
+        Intent intent = new Intent(MainActivity.this,Colors.class);
+        startActivity(intent);
+    }
+
+    public void Perm_check(){
         if (ContextCompat.checkSelfPermission(getBaseContext(),
                 Manifest.permission.READ_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -57,11 +72,60 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
+
+
     }
 
-    public void colorOnclickListner(View view) {
-        Intent intent = new Intent(MainActivity.this,Colors.class);
-        startActivity(intent);
+    public void Perm_bluetooth(){
+        if (ContextCompat.checkSelfPermission(getBaseContext(),
+                Manifest.permission.BLUETOOTH)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.BLUETOOTH)) {
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.BLUETOOTH},3);
+
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(getBaseContext(),
+                Manifest.permission.BLUETOOTH_ADMIN)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.BLUETOOTH_ADMIN)) {
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.BLUETOOTH_ADMIN},3);
+
+            }
+        }
+    }
+
+    public void blue_init(){
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetooth == null)
+        {
+            //Show a mensag. that thedevice has no bluetooth adapter
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+            //finish apk
+            finish();
+        }
+        else
+        {
+            if (myBluetooth.isEnabled())
+            { }
+            else
+            {
+                //Ask to the user turn the bluetooth on
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon,1);
+            }
+        }
     }
 }
 
